53fb247bd2ffed4330c11b5a1dac1f16150648e9
Dmytro Bondar <git@bonddim.dev>
2025-09-30 16:14:44 +0200

    Ignore role_assignments tflint rule for variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# tflint-ignore-file: role_assignments
 variable "location" {
   type        = string
   description = "Azure region where the resource should be deployed.  If null, the location will be inferred from the resource group location."
