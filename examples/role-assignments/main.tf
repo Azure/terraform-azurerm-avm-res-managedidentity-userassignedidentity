247923d188fffc00be72dda8ea8df7681453834b
Paul BOURHIS <43958209+Pyodin@users.noreply.github.com>
2026-03-16 21:59:03 +0100

    Remove deprecated resource_group_name from federated identity credential resource and add examples for federated credentials and role assignments with telemetry support (#106)
    Co-authored-by: Paul Bourhis <paul.bourhis@bhs-consulting.com>

---
+++ examples/role-assignments/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+## Section to provide a random Azure region for the resource group
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/regions/azurerm"
+  version = "0.8.2"
+}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+## End of section to provide a random Azure region for the resource group
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.4.2"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+# This is the module call.
+module "test" {
+  source = "../../"
+
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.user_assigned_identity.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  enable_telemetry    = var.enable_telemetry # see variables.tf
+  role_assignments = {
+    reader_on_resource_group = {
+      role_definition_id_or_name = "Reader"
+      scope                      = azurerm_resource_group.this.id
+      description                = "Reader access on the example resource group"
+    }
+  }
+}
